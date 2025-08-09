f26e80bd468e26da4139c75bea9c0b5d9776c659
Rhys Maddren <rhysmaddren@hotmail.co.uk>
2025-08-09 13:15:42 +0100

    Add book class

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        /// <summary>
+        /// The book identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the book.
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The author identifier for the book.
+        /// </summary>
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// The year the book was published.
+        /// </summary>
+        public int PublishedYear { get; set; }
+
+        /// <summary>
+        /// The International Standard Book Number (ISBN) of the book.
+        /// </summary>
+        public string ISBN { get; set; } = string.Empty;
+    }
+}
